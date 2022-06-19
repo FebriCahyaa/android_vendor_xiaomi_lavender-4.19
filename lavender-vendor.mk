3d41ff9095b77ab216a493d3b3362040499ca9b8
Aaradhay Vijay <inferno964.github@gmail.com>
2022-06-19 17:14:45 +0530

    lavender: Pull fingerprint blobs from lavender-user-10-QKQ1.190910.002-V12.5.3.0.QFGMIXM-release-keys
    Change-Id: Ie3d59090cfb44664299ad280c16b8ae420b38363

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1014,3 +1014,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libthermalclient \
     manifest_android.hardware.drm@1.2-service.widevine
+    vendor/xiaomi/lavender/proprietary/vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.fingerprint@1.0.so
+
+PRODUCT_PACKAGES += \
+    FingerprintExtensionService
