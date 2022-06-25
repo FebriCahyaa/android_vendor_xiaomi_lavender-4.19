c97d8215204828caa600066975fc9824f661d866
alienatiz <kbc20000@gmail.com>
2022-06-25 22:09:36 +0530

    lavender: Kang sensors.ssc from Nokia PL2
    - Fixes deep sleep
    - From PL2_sprout-user 10 QKQ1.190828.002 00WW_4_150
    
    [Original commit]: https://review.lineageos.org/c/LineageOS/android_device_xiaomi_whyred/+/297914
    [Kim Byeongcheol]: The stock snsors.ssc.so of lavender broke deep sleep, with the sensors.ssc of Nokia PL2 works fine.
    
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>
    Signed-off-by: Kim Byeongcheol <kbc20000@gmail.com>
    Change-Id: I7658e76bddeab3e0d8e0410c5b190cd6fcdde05d

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -989,6 +989,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/lavender/proprietary/vendor/lib/mibokeh_660_opencl.bin:$(TARGET_COPY_OUT_VENDOR)/lib/mibokeh_660_opencl.bin \
     vendor/xiaomi/lavender/proprietary/vendor/lib/sensors.ssc.so:$(TARGET_COPY_OUT_VENDOR)/lib/sensors.ssc.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libwvdrmengine.so \
+    vendor/xiaomi/lavender/proprietary/vendor/lib/vendor.qti.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.fingerprint@1.0.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/com.fingerprints.extension@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.fingerprints.extension@2.0.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/hw/activity_recognition.sdm660.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/activity_recognition.sdm660.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
